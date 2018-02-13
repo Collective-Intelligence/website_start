01568e0624120fb180604f1b88ae0e1473a8b309
Trevor Hoglund <trewbot@phene.co>
2018-02-13 14:13:05 -0600

    Added user schema

---
+++ models/user.js
@@ -0,0 +1,36 @@
+/**
+ *  User Settings Schema
+ *  @author Una Ada (Trewbot) <una@phene.co>
+ *  This file isn't meant as operable code, but as a descriptor of what info-
+ *  -rmation needs to be able to be derived from requests to the server from the
+ *  client JavaScript. Preferably, this would be sent as a JSON string such that
+ *  the client would be able to parse this into a JavaScript object and save it
+ *  as a session state.
+ */
+
+//  Subtypes
+const Column = {
+    contains: [String],     //  List of moduleids contained in the column
+    width: Number           //  Width of the column in pixels
+};
+const Token = {
+    perm: String
+    temp: String
+};
+
+//  User schema
+const User = {
+    account: String,
+    adToken: Token,
+    display: String,        //  Name to be displayed for the user
+    experience: Number,
+    gp: Number,
+    owed: Number,
+    type: "Account",
+    ui: {
+        accent: String      //  Accent color for display
+        columns: [Column]   //  Columns to be rendered in the client
+    },
+    upvoteToken: Token,
+    username: String        //  Internal name for user (possibly displayed)
+};
